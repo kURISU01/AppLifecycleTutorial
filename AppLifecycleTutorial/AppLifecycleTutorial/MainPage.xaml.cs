35ad127470e69e293d90161d031ec410a152b4ee
makise_kurisu04@mail.ru <makise_kurisu04@mail.ru>
2021-12-08 08:15:27 +0300

    Add project files.

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppLifecycleTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            entry.Text = (Application.Current as App).DisplayText;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            (Application.Current as App).DisplayText = entry.Text;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
